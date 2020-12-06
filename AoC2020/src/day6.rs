e14247bd5e997bf06ada238a073adcd5e2624e60
DeltaManiac <maxpaynered@gmail.com>
2020-12-06 10:54:53 +0530

    Day 6

---
+++ AoC2020/src/day6.rs
@@ -0,0 +1,37 @@
+use std::collections::HashSet;
+#[aoc(day6, part1)]
+fn solve_part1(input: &str) -> usize {
+    input
+        .split("\n\n")
+        .map(|g| {
+            g
+                .lines()
+                .map(|l| l.chars())
+                .flatten()
+                .collect::<HashSet<_>>()
+                .len()
+        })
+        .sum()
+}
+
+#[aoc(day6,part2)]
+fn solve_part2(input:&str)-> usize{
+input
+        .split("\n\n")
+        .map(|g| {
+            g
+                .lines()
+                .fold(None, |a, l| {
+                    let c: HashSet<_> = l.chars().collect();
+                    if let Some(a) = a {
+                        Some(c.intersection(&a).copied().collect())
+                    } else {
+                        Some(c)
+                    }
+                })
+                .unwrap()
+                .len()
+        })
+        .sum()
+
+}
