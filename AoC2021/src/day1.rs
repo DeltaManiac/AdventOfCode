e8dc5afcb912fc5e93620637f73347625ccaa925
DeltaManiac <maxpaynered@gmail.com>
2021-11-26 19:49:05 +0530

    AoC2021 Cleanup

---
+++ AoC2021/src/day1.rs
@@ -0,0 +1,16 @@
+use itertools::Itertools;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    input.lines().map(|l| l.parse::<u32>().unwrap()).collect()
+}
+
+#[aoc(day1, part1, 3sum)]
+pub fn solve_part1(input: &[u32]) -> u32 {
+    0
+}
+
+#[aoc(day1, part2, 3sum)]
+pub fn solve_part2(input: &[u32]) -> u32 {
+    0
+}
