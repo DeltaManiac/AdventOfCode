671c025277ec5c27bffc7cb747044b838538f8a3
Harikrishnan Menon <DeltaManiac@users.noreply.github.com>
2021-12-01 11:22:45 +0530

    Day1 Solution

--- AoC2021/src/day1.rs
+++ AoC2021/src/day1.rs
@@ -6,11 +6,17 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 }
 
 #[aoc(day1, part1, 3sum)]
-pub fn solve_part1(input: &[u32]) -> u32 {
-    0
+pub fn solve_part1(input: &[u32]) -> usize {
+    input.iter().tuple_windows().filter(|(a, b)| b > a).count()
 }
 
 #[aoc(day1, part2, 3sum)]
-pub fn solve_part2(input: &[u32]) -> u32 {
-    0
+pub fn solve_part2(input: &[u32]) -> usize {
+    input
+        .iter()
+        .tuple_windows()
+        .map(|(a, b, c)| a + b + c)
+        .tuple_windows()
+        .filter(|(a, b)| b > a)
+        .count()
 }
