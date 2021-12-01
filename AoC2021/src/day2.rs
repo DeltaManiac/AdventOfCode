3224968d3714cbfb46b3030be4ee81f7129ba806
DeltaManiac <maxpaynered@gmail.com>
2021-12-01 19:26:04 +0530

    AoC 2021 Day 1

--- AoC2021/src/day2.rs
+++ AoC2021/src/day2.rs
@@ -7,16 +7,11 @@ pub fn input_generator(input: &str) -> Vec<u32> {
 
 #[aoc(day1, part1 )]
 pub fn solve_part1(input: &[u32]) -> usize {
-    input.iter().tuple_windows().filter(|(a, b)| b > a).count()
+    0
 }
 
-#[aoc(day1, part2)]
+#[aoc(day1, part2 )]
 pub fn solve_part2(input: &[u32]) -> usize {
-    input
-        .iter()
-        .tuple_windows()
-        .map(|(a, b, c)| a + b + c)
-        .tuple_windows()
-        .filter(|(a, b)| b > a)
-        .count()
+    0
 }
+
