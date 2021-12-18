e686399a016c07b9c4c7b29f75f3b3f5d673431c
DeltaManiac <maxpaynered@gmail.com>
2021-12-18 10:32:31 +0530

    AoC 2021 Day 17

--- AoC2021/src/main.rs
+++ AoC2021/src/main.rs
@@ -6,6 +6,7 @@ mod day13;
 mod day14;
 mod day15;
 mod day16;
+mod day17;
 mod day2;
 mod day3;
 mod day4;
@@ -32,4 +33,5 @@ aoc_main::main! {
     day14 :input_generator=> solve_part1, solve_part2;
     day15 :input_generator=> solve_part1, solve_part2;
     day16 :input_generator=> solve_part1, solve_part2;
+    day17 :input_generator=> solve_part1, solve_part2;
 }
